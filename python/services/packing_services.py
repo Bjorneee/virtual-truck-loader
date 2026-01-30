16297e5489088a48aeccc90a9fabce54acd09a0a
Bjorneee <benicimo4@gmail.com>
2026-01-30 14:09:23 -0800

    Added 1DFFD, updated data models, and added object instatiation in packing service

--- python/services/packing_services.py
+++ python/services/packing_services.py
@@ -1,13 +1,35 @@
 import time
 from python.vtl_core.schemas import PackingRequest, PackingResponse, PlacedBox
-
-placed = []
-unplaced = []
+from python.vtl_core.domain import models
 
 def run_packing(req: PackingRequest) -> PackingResponse:
     start = time.time()
 
     # TODO: replace with real packing algorithm
+
+    # Instantiate truck object
+    truck = models.Truck(
+        id=req.truck.id,
+        width=req.truck.width,
+        height=req.truck.height,
+        depth=req.truck.depth,
+        max_weight=req.truck.max_weight
+    )
+
+    # Instantiate all boxes into unplaced list
+    unplaced = []
+    for box in req.boxes:
+        unplaced.append(models.Box(
+            id=box.id,
+            width=box.width,
+            height=box.height,
+            depth=box.depth,
+            weight=box.weight,
+            rotatable=box.rotatable,
+            priority=box.priority
+        ))
+
+    placed = []
     for box in req.boxes:
         placed.append(PlacedBox(
             id=box.id, 
--- python/vtl_core/packing/heurisitics.py
+++ python/vtl_core/packing/heurisitics.py
@@ -10,3 +10,41 @@ Currently implemented heuristics:
 // For Testing
 - First Fit Decreasing (FFD)
 """
+
+from typing import List
+
+"""
+First-Fit Decreasing bin packing.
+
+Args:
+    items: list of item sizes (must be <= bin_capacity)
+    bin_capacity: capacity of each bin
+
+Returns:
+    A list of bins, each bin is a list of item sizes
+"""
+def ffd_1d(items: List[float], bin_capacity: float) -> List[List[float]]:
+
+    # Sort items in decreasing order
+    items = sorted(items, reverse=True)
+
+    bins: List[List[float]] = []
+    remaining_capacity: List[float] = []
+
+    for item in items:
+        placed = False
+
+        # Try to place item in the first bin that fits
+        for i in range(len(bins)):
+            if remaining_capacity[i] >= item:
+                bins[i].append(item)
+                remaining_capacity[i] -= item
+                placed = True
+                break
+
+        # If no bin fits, open a new bin
+        if not placed:
+            bins.append([item])
+            remaining_capacity.append(bin_capacity - item)
+
+    return bins
